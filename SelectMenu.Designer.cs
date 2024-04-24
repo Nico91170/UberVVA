e8f4dfb23e636ce882ce9d7bbdcfa1e0c874f1e3
tristanplsrd <114140162+tristanplsrd@users.noreply.github.com>
2024-04-24 16:54:26 +0200

    modfi

--- SelectMenu.Designer.cs
+++ SelectMenu.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectionMenu));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -46,6 +47,15 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "On vous souhaite un bon appétit d\'avance !";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(428, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
             // SelectionMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -53,6 +63,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(913, 461);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "SelectionMenu";
             this.Text = "Selection des Menus";
@@ -64,5 +75,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
