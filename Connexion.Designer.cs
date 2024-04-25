59b490c27b7c75e48ff72e2fa4057019ccdeaa6a
tristanplsrd <114140162+tristanplsrd@users.noreply.github.com>
2024-04-25 11:48:51 +0200

    Modif

--- Connexion.Designer.cs
+++ Connexion.Designer.cs
@@ -31,10 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConn));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.tbU = new System.Windows.Forms.TextBox();
-            this.tbPW = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpwd = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -61,21 +63,22 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Veuillez bien vouloir vous connectez \r\n          pour pouvoir déguster !";
             // 
-            // tbU
+            // txtuser
             // 
-            this.tbU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.tbU.Location = new System.Drawing.Point(182, 214);
-            this.tbU.Name = "tbU";
-            this.tbU.Size = new System.Drawing.Size(172, 31);
-            this.tbU.TabIndex = 2;
+            this.txtuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtuser.Location = new System.Drawing.Point(182, 214);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(172, 31);
+            this.txtuser.TabIndex = 2;
             // 
-            // tbPW
+            // txtpwd
             // 
-            this.tbPW.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
-            this.tbPW.Location = new System.Drawing.Point(182, 298);
-            this.tbPW.Name = "tbPW";
-            this.tbPW.Size = new System.Drawing.Size(172, 31);
-            this.tbPW.TabIndex = 3;
+            this.txtpwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.txtpwd.Location = new System.Drawing.Point(182, 298);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.PasswordChar = '*';
+            this.txtpwd.Size = new System.Drawing.Size(172, 31);
+            this.txtpwd.TabIndex = 3;
             // 
             // label3
             // 
@@ -97,16 +100,37 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "password";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Se Connecter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Abandonner";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FormConn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(554, 453);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.tbPW);
-            this.Controls.Add(this.tbU);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtuser);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FormConn";
@@ -121,10 +145,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbU;
-        private System.Windows.Forms.TextBox tbPW;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpwd;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Connexion.cs
+++ Connexion.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace UberVVA
 {
@@ -20,6 +21,41 @@ namespace UberVVA
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connexion = new MySqlConnection("database = foodmaps; server=localhost; user id=root; pwd=");
+
+
+            try
+            {
+                connexion.Open();
+                string Query = "select * FROM user WHERE username='" + txtuser.Text.Trim() + "'AND password='" + txtpwd.Text.Trim() + "'";
+                MySqlDataAdapter sda = new MySqlDataAdapter(Query, connexion);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count == 1)
+                {
+                    SelectionMenu dr = new SelectionMenu();
+                    dr.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Les identifiants sont faux");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
